49699e65577e893928d9dce84875c9c0f1f398e0
Raphael  Alexis do Nascimento <alexis.raphael95@gmail.com>
2023-08-10 00:48:39 -0300

    commit initial

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,18 @@
+
+
+import { PostsProvider } from '../../contexts/PostsProvider';
+import { Posts } from '../../components/Posts';
+import { CounterProvider } from '../../contexts/CounterProvider';
+
+export function App() {
+  return (
+    <CounterProvider>
+    <PostsProvider>
+      <div>
+        <Posts />
+      </div>
+    </PostsProvider>
+    </CounterProvider>
+  );
+}
+
