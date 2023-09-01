fd93d77bd2d52cd65da8d35d29d0d58460a348ee
Raphael  Alexis do Nascimento <alexis.raphael95@gmail.com>
2023-09-01 02:30:56 -0300

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './templates/App';
-import { Home } from './custom-hooks';
+import { Home2 } from './useAsync';
+import { Home3 } from './useLayoutEffect';
+import { Home4 } from './compound-components';
 
 
 
@@ -9,6 +10,6 @@ import { Home } from './custom-hooks';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Home4 />
   </React.StrictMode>
 );
